4456115cb973a307df01789ed6fe09b10fc5c29a
Aman Kumar Gupta <gupta2140@gmail.com>
2018-10-11 13:43:42 +0530

    Updated comments in liveness.py
    Signed-off-by: Aman Kumar Gupta <gupta2140@gmail.com>

--- custom-client/liveness.py
+++ custom-client/liveness.py
@@ -4,11 +4,11 @@ import time
 import os
 
 # Assigning the environment variables
-i_w_d = os.environ['INIT_WAIT_DELAY']   # Time period (in sec) b/w retries for DB init check
-i_r_c = os.environ['INIT_RETRY_COUNT']      # No of retries for DB init check
-l_p_s = os.environ['LIVENESS_PERIOD_SECONDS'] # Time period (in sec) b/w liveness checks
-l_t_s = os.environ['LIVENESS_TIMEOUT_SECONDS']  # Time period (in sec) b/w retries for db_connect failure
-l_r_c = os.environ['LIVENESS_RETRY_COUNT'] # No of retries after a db_connect failure before declaring liveness fail
+i_w_d = os.environ['INIT_WAIT_DELAY']   # Time period (in sec) between retries for DB init check
+i_r_c = os.environ['INIT_RETRY_COUNT']      # Number of retries for DB init check
+l_p_s = os.environ['LIVENESS_PERIOD_SECONDS'] # Time period (in sec) between liveness checks
+l_t_s = os.environ['LIVENESS_TIMEOUT_SECONDS']  # Time period (in sec) between retries for db_connect failure
+l_r_c = os.environ['LIVENESS_RETRY_COUNT'] # Number of retries after a db_connect failure before declaring liveness fail
 port = os.environ['PORT']
 ns = os.environ['NAMESPACE']
 sv = os.environ['SERVICE']
