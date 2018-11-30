bb225b00d2c2b3502a663acc529ed507284abf03
ksatchit <karthik.s@openebs.io>
2018-11-30 09:29:39 +0000

    (refactor)(image): enhance liveness load w/ DB CRUD ops
    Signed-off-by: ksatchit <karthik.s@openebs.io>

--- custom-percona/sql-test.sh
+++ custom-percona/sql-test.sh
@@ -19,7 +19,27 @@ then
   echo -e "\nFailed to connect to db server after trying for $(($retries * $wait_retry))s, exiting\n"
   exit 1
 fi
+
+## DB CREATE 
 mysql -uroot -pk8sDem0 -e "CREATE DATABASE $DB_NAME;"
 mysql -uroot -pk8sDem0 -e "CREATE TABLE Hardware (id INTEGER, name VARCHAR(20), owner VARCHAR(20),description VARCHAR(20));" $DB_NAME
-mysql -uroot -pk8sDem0 -e "INSERT INTO Hardware (id, name, owner, description) values (1, "dellserver", "basavaraj", "controller");" $DB_NAME
+
+## DB WRITE
+for i in {1..100}; do 
+  tvalue=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 18 | head -n 3`; arr=($tvalue)
+  mysql -uroot -pk8sDem0 -e "INSERT INTO Hardware (id, name, owner, description) values (${i}, "${arr[0]}", "${arr[1]}", "${arr[2]}");" $DB_NAME
+  sync; 
+done
+
+## DB UPDATE
+for i in {1..50}; do
+  mysql -uroot -pk8sDem0 -e "UPDATE Hardware SET description ="master" WHERE id = $i;" $DB_NAME 
+  sync; 
+done
+
+## DB PARALLEL READ/WRITE
+mysql -uroot -pk8sDem0 -e "INSERT INTO Hardware SELECT * FROM Hardware;" $DB_NAME 
+
+## DB DESTROY
 mysql -uroot -pk8sDem0 -e "DROP DATABASE $DB_NAME;"
+sync; 
