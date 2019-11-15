ea7435578675ff282772ec2b3d464f71cc409bb9
Karthik Satchitanand <karthik.s@mayadata.io>
2019-11-15 17:45:21 +0530

    (refactor)kafka_scripts: update scripts to use less pipes and add kafka timeout (#103)
    Signed-off-by: ksatchit <ksatchit@mayadata.io>

--- kafka-client/producer.sh
+++ kafka-client/producer.sh
@@ -2,7 +2,6 @@
 
 i=0
 while true; do 
-  raw_message=$(echo "** record_index=${i}" | ts '[%Y-%m-%d %H:%M:%S]')
-  echo "** producer_ts: ${raw_message}" | kafka-console-producer --broker-list ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME}
+  echo "message_index ${i} produced @ $(date -u)" | kafka-console-producer --broker-list ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME}
   ((i++)) 
 done
