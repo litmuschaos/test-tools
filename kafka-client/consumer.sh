0a4696ba0fbce5862c4d4957ef59a6a9151efc12
ksatchit <ksatchit@mayadata.io>
2019-11-15 16:24:07 +0530

    (refactor)kafka_scripts: update scripts to use less pipes and add kafka timeout
    Signed-off-by: ksatchit <ksatchit@mayadata.io>

--- kafka-client/consumer.sh
+++ kafka-client/consumer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kafka-console-consumer --bootstrap-server ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME} | ts '[%Y-%m-%d %H:%M:%S]' | ts 'consumer_ts:' 
+kafka-console-consumer --bootstrap-server ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME} --timeout-ms ${KAFKA_CONSUMER_TIMEOUT} | ts '[%Y-%m-%d %H:%M:%S]' 
 
 
 
