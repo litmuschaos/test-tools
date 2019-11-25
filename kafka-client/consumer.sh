1699341c24e865a8b16748357e39b0040453ed59
ksatchit <ksatchit@mayadata.io>
2019-11-26 04:18:38 +0530

    (refactor)auth: support kafka auth in client container
    Signed-off-by: ksatchit <ksatchit@mayadata.io>

--- kafka-client/consumer.sh
+++ kafka-client/consumer.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-kafka-console-consumer --bootstrap-server ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME} --timeout-ms ${KAFKA_CONSUMER_TIMEOUT} | ts '[%Y-%m-%d %H:%M:%S]' 
+if [ ! -z "${KAFKA_OPTS}" ]; then
+  kafka-console-consumer --bootstrap-server ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME} --timeout-ms ${KAFKA_CONSUMER_TIMEOUT} --consumer.config /opt/client.properties | ts '[%Y-%m-%d %H:%M:%S]' 
+else
+  kafka-console-consumer --bootstrap-server ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME} --timeout-ms ${KAFKA_CONSUMER_TIMEOUT} | ts '[%Y-%m-%d %H:%M:%S]'
+fi
+
+
 
 
 
--- kafka-client/producer.sh
+++ kafka-client/producer.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 i=0
-while true; do 
-  echo "message_index ${i} produced @ $(date -u)" | kafka-console-producer --broker-list ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME}
+while true; do
+  if [ ! -z "${KAFKA_OPTS}" ]; then 	
+    echo "message_index ${i} produced @ $(date -u)" | kafka-console-producer --broker-list ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME} --producer.config /opt/client.properties
+  else
+    echo "message_index ${i} produced @ $(date -u)" | kafka-console-producer --broker-list ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME}
+  fi
+
   ((i++)) 
 done
