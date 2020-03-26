e0e8a60c420217531478b53ab4be1842dcc19a9e
Raj Babu Das <mail.rajdas@gmail.com>
2020-03-26 16:21:09 +0530

    Upgrading python version to 3 (#120)
    Signed-off-by: Raj <raj.das@mayadata.io>

--- app_clients/nfs-client/nfs-mount-liveness-check.py
+++ app_clients/nfs-client/nfs-mount-liveness-check.py
@@ -22,47 +22,49 @@ nfs_svc_ip = os.environ['NFS_SVC_IP']
 This function checks, if the volume is present in the mounted volume list.
 The volume list is generating using `showmount` command
 Respose:
-	True: If volume founds in the volume list
-	False: Not found
+  True: If volume founds in the volume list
+  False: Not found
 """
 def isMount(ip, volume):
-	commands = ["showmount", "-e", ip, "--no-headers"]                         
-	volume_list=subprocess.check_output(commands, timeout=command_timeout) 
+  commands = ["showmount", "-e", ip, "--no-headers"]                         
+  volume_list=subprocess.check_output(commands, timeout=int(command_timeout)) 
 
-	if volume_list.decode("utf-8").find(volume) >= 0:
-		return True
-	return False
+  if volume_list.decode("utf-8").find(volume) >= 0:
+    return True
+  return False
 
 def reCheck(ip, volume):
-	"""
-	Retries int(l_r_c) times with int(l_t_s) secs wait between each retry
-	"""
-	for y in range(1, int(l_r_c)):
-		z = isMount(ip, volume)
-		if z is True:
-				return True
-		if z is False:
-			print("Rechecking")
-			time.sleep(int(l_t_s))
-	return False
+  """
+  Retries int(l_r_c) times with int(l_t_s) secs wait between each retry
+  """
+  for y in range(1, int(l_r_c)):
+    z = isMount(ip, volume)
+    if z is True:
+      return True
+    if z is False:
+      print("Rechecking", flush=True)
+      time.sleep(int(l_t_s))
+  return False
 
 def liveness(ip, volume):
-	"""
-	Wait int(l_p_s) secs between each retry with max retry of int(l_r_c)
-	"""
-	while True:
-		res = isMount(ip, volume)
-		if res is True:
-				print("volume is exported")
-		else:
-				print("volume is not exported")
-				rc = reCheck(ip, volume)
-				if rc is False:
-					print("Liveness finally failed:")
-					break
-		time.sleep(int(l_p_s))
+  """
+  Wait int(l_p_s) secs between each retry with max retry of int(l_r_c)
+  """
+  while True:
+    res = isMount(ip, volume)
+    if res is True:
+        print("volume is exported", flush=True)
+    else:
+        print("volume is not exported", flush=True)
+        rc = reCheck(ip, volume)
+        if rc is False:
+          print("Liveness finally failed:", flush=True)
+          break
+    time.sleep(int(l_p_s))
 
 if __name__ == '__main__':
-	time.sleep(int(i_w_d))
-	ip = nfs_svc_ip
-	liveness(ip, volume)
+  print('Waiting for initial wait delay ...', flush=True)
+  time.sleep(int(i_w_d))
+  print('Starting the liveness check ...', flush=True)
+  ip = nfs_svc_ip
+  liveness(ip, volume)
