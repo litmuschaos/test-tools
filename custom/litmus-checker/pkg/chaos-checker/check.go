7120ac58f299b20e0f3892479c86af76416945f9
Soumya Ghosh Dastidar <44349253+gdsoumya@users.noreply.github.com>
2020-09-23 13:28:11 +0530

    Added Litmus Checker (#141)
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

---
+++ custom/litmus-checker/pkg/chaos-checker/check.go
@@ -0,0 +1,34 @@
+package chaos_checker
+
+import (
+	"log"
+	"strings"
+	"time"
+
+	"github.com/gdsoumya/resourceChecker/pkg/k8s"
+)
+
+func CheckChaos(kubeconfig *string, res k8s.ResourceDef) {
+	dc, dyn, err := k8s.GetDynamicClient(kubeconfig)
+	if err != nil {
+		log.Fatal("ERROR : ", err)
+	}
+	log.Print("Starting Chaos Checker in 1min")
+	for {
+		time.Sleep(time.Minute * 1)
+		log.Print("Checking if Engine Completed or Stopped")
+		data, err := k8s.GetResourceDetails(dc, dyn, res)
+		if err != nil {
+			log.Fatal("ERROR : ", err)
+		}
+		status, ok := data.Object["status"].(map[string]interface{})
+		if !ok {
+			continue
+		}
+		engStat, ok := status["engineStatus"].(string)
+		if ok && (strings.ToLower(engStat) == "completed" || strings.ToLower(engStat) == "stopped") {
+			log.Print("ENGINE COMPLETED")
+			return
+		}
+	}
+}
