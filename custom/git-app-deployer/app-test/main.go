0e6ea83209c257f1f9bf34de88803f5b5eee6cea
oumkale <oum.kale@mayadata.io>
2021-01-15 17:00:32 +0530

    litmus-qps-cmd
    Signed-off-by: oumkale <oum.kale@mayadata.io>

--- custom/git-app-deployer/app-test/main.go
+++ custom/git-app-deployer/app-test/main.go
@@ -15,106 +15,112 @@ import (
 	"github.com/litmuschaos/test-tools/pkg/log"
 )
 
-var (
-	timeSumsMu sync.RWMutex
-	timeSums   int64
-	qps        string
-	prevReq    int
-	sum        int
-	totalCount string
-)
+//QPSVars will carry all the params for functionality of end point
+type QPSVars struct {
+	timeSumsMu    sync.RWMutex
+	totalQueries  int
+	qpsValue      string
+	totalReqCount string
+}
 
 func main() {
 	log.Info("[Status]: Starting QPS provider...")
-	go runDataLoop()
 
+	qpsVars := initialiseVars()
+
+	go runDataLoop(qpsVars)
+
+	// Create a handler that will read-lock the mutext and
+	// write the summed time to the end point
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		timeSumsMu.RLock()
-		defer timeSumsMu.RUnlock()
-		fmt.Fprint(w, qps)
+		qpsVars.timeSumsMu.RLock()
+		defer qpsVars.timeSumsMu.RUnlock()
+		fmt.Fprint(w, qpsVars.qpsValue)
 	})
 	http.ListenAndServe(":8080", nil)
 }
 
-//// Start the goroutine that will sum the current time
-// once per second.
-// Create a handler that will read-lock the mutext and
-// write the summed time to the client
-func runDataLoop() {
+//initialiseVars is initialling required variables for app-test
+//totalQueries is keeping count of requests in given time interval
+//qpsValue is Queries per second
+//totalReqCount is total requests to endpoint
+func initialiseVars() (vars *QPSVars) {
+	qpsVars := QPSVars{
+		totalQueries:  0,
+		qpsValue:      "0",
+		totalReqCount: "0",
+	}
+	return &qpsVars
+}
+
+// It will count the mean value of total queries available in given time interval and update
+// end point qith query per second.
+func runDataLoop(vars *QPSVars) {
 	queue := list.New()
-	timeInt := os.Getenv("TIME") //The time period in second to calculate mean value of QPS.
-	url := os.Getenv("URL")      //URL of endpoint metics
-	route := os.Getenv("ROUTE")  //route is a endpoint of application to get qps
+
+	timeInterval, _ := strconv.Atoi(os.Getenv("TIME")) //The time period in second to calculate mean value of QPS.
+	url := os.Getenv("URL")                            //URL of endpoint metics
+	route := os.Getenv("ROUTE")                        //route is a endpoint of application to get qpsValue
 	flag.Parse()
 
-	timeOfInt, _ := strconv.Atoi(timeInt)
-
+	// Within an infinite loop, lock the mutex and
+	// increment our value, then sleep for 1 second until
+	// the next time we need to get a value.
+	start := time.Now()
 	for {
-		// Within an infinite loop, lock the mutex and
-		// increment our value, then sleep for 1 second until
-		// the next time we need to get a value.
-		start := time.Now()
-		timeSums += time.Now().Unix()
-		prevReq = 0
-		for i := 1; ; i++ {
-			req, err := GetRequests(url, route)
-			if err != nil {
-				qps = strconv.Itoa(0)
-				log.Errorf("Failed to fetch responce, err: %v", err)
-			}
 
-			second := int(time.Now().Sub(start).Seconds())
-			reqs, _ := strconv.Atoi(req)
-			sum = reqs
-
-			if second < timeOfInt+1 {
-				qps = string(strconv.Itoa(reqs - prevReq))
-				queue.PushBack(reqs)
-			} else {
-				front := queue.Front()
-				queue.Remove(front)
-				queue.PushBack(reqs)
-				sum -= front.Value.(int)
-				qps = string(strconv.Itoa(sum / timeOfInt))
-			}
-			prevReq = reqs
-
-			log.Infof("[Status]: Current total requests : ", req)
-			log.Infof("[Status]: Current QPS value is : ", qps)
+		req, err := GetRequests(url, route, vars)
+		if err != nil {
+			log.Errorf("err: %v", err)
+			vars.qpsValue = strconv.Itoa(0)
+			time.Sleep(1 * time.Second)
+			continue
 		}
+
+		second := int(time.Now().Sub(start).Seconds())
+		reqs, _ := strconv.Atoi(req)
+		vars.totalQueries = reqs
+
+		if second <= timeInterval {
+			queue.PushBack(reqs)
+			vars.qpsValue = strconv.Itoa(100 * (reqs / queue.Len()))
+		} else {
+			front := queue.Front()
+			queue.Remove(front)
+			queue.PushBack(reqs)
+			vars.totalQueries -= front.Value.(int)
+			vars.qpsValue = strconv.Itoa(100 * (vars.totalQueries / timeInterval))
+		}
+		log.Infof("[Status]: Current total requests : ", req)
+		log.Infof("[Status]: Current QPS value is   : ", vars.qpsValue)
+		time.Sleep(1 * time.Second)
 	}
 }
 
 //GetRequests will fetch the responce from metrics and calculate the total requests from front-end of sock-shop.
-func GetRequests(url string, route string) (string, error) {
+func GetRequests(url string, route string, vars *QPSVars) (string, error) {
 
-	time.Sleep(1 * time.Second)
 	response, err := http.Get(url)
 	if err != nil {
-		qps = strconv.Itoa(0)
-		log.Errorf("Failed to fetch responce, err: %v", err)
 		return "", err
-	} else {
-		defer response.Body.Close()
-		metric, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Errorf("Failed to read responce, err: %v", err)
-			return "", err
-		}
-
-		metrics := string(metric)
-
-		value := strings.Split(metrics, "\n")
-		flag := 0
-		for i := 0; i < len(value) && flag == 0; i++ {
-
-			if true == strings.Contains(string(value[i]), `request_duration_seconds_count{service="front-end",method="get",route="`+route+`",status_code="200`) {
-				val := strings.Split(value[i], " ")
-				totalCount = val[1]
-				flag = 1
-			}
-		}
-
 	}
-	return totalCount, nil
+	defer response.Body.Close()
+
+	metric, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	metrics := string(metric)
+	metricsSplited := strings.Split(metrics, "\n")
+
+	for i := 0; i < len(metricsSplited); i++ {
+		if strings.Contains(string(metricsSplited[i]), `request_duration_seconds_count{service="front-end",method="get",route="`+route+`",status_code="200`) {
+			metricsValue := strings.Split(metricsSplited[i], " ")
+			vars.totalReqCount = metricsValue[1]
+			return vars.totalReqCount, nil
+		}
+	}
+
+	return vars.totalReqCount, fmt.Errorf("Provided route is incorrect")
 }
