18b12d65f84d6eade1fcf82a26e669ca4d82f13c
Oum Kale <oumkale@chaosnative.com>
2021-09-20 12:23:59 +0530

    jmeter script for postgresql
    Signed-off-by: Oum Kale <oumkale@chaosnative.com>

--- custom/workflow-helper/postgres-helper/jmeter/shell.sh
+++ custom/workflow-helper/postgres-helper/jmeter/shell.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 echo "Jmeter Query Load Generation For PostgreSQL Application"
-echo $PASSWORD
-echo $USERNAME
 echo $DATA_SOURCE_URI
 
 sh ./jmeter -JpasswordU=$PASSWORD -Jusername=$USERNAME -JdsURI=$DATA_SOURCE_URI -n -t postgres-load.jmx -l result.jtl
