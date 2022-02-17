70478edb4b45a0758f62ad437b7697c4f83ff3f4
tuannvm <me@tuannvm.com>
2022-02-17 16:07:54 +0800

    Use kafka bootstrap instead of zookeeper

--- app_clients/kafka-client/topic.sh
+++ app_clients/kafka-client/topic.sh
@@ -3,17 +3,11 @@
 now="$(date +'%m/%d/%Y') $(date +"%T")"
 echo "$now: [Info] Starting topic"
 printf "$now: [Info] The topic information is as follow:
-      Zookeeper Service: ${ZOOKEEPER_SERVICE}
-      Zookeeper Port: ${ZOOKEEPER_PORT}
-      Kafka Instance Name: ${KAFKA_INSTANCE_NAME}
+      Kafka Service: ${KAFKA_SERVICE}
+      Kafka Port: ${KAFKA_PORT}
       Topic Name: ${TOPIC_NAME}
       Replication Factor: ${REPLICATION_FACTOR}
       "
-if [ ! -z "${KAFKA_INSTANCE_NAME}" ]; then
-  zk_uri=$(echo ${ZOOKEEPER_SERVICE}:${ZOOKEEPER_PORT}/${KAFKA_INSTANCE_NAME})
-else
-  zk_uri=$(echo ${ZOOKEEPER_SERVICE}:${ZOOKEEPER_PORT})
-fi
 
-output=$(kafka-topics --zookeeper ${zk_uri} --topic ${TOPIC_NAME} --create --partitions 1 --replication-factor ${REPLICATION_FACTOR} --if-not-exists)
+output=$(kafka-topics --bootstrap-server ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME} --create --partitions 1 --replication-factor ${REPLICATION_FACTOR} --if-not-exists)
 echo "$now: $output"
