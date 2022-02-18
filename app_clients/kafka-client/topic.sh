bba7467a6c1e99ea8e09936c647bcad27da31909
tuannvm <me@tuannvm.com>
2022-02-18 17:11:03 +0800

    Adjust topic
    Signed-off-by: tuannvm <me@tuannvm.com>

--- app_clients/kafka-client/topic.sh
+++ app_clients/kafka-client/topic.sh
@@ -5,9 +5,15 @@ echo "$now: [Info] Starting topic"
 printf "$now: [Info] The topic information is as follow:
       Kafka Service: ${KAFKA_SERVICE}
       Kafka Port: ${KAFKA_PORT}
+      Kafka Instance Name: ${KAFKA_INSTANCE_NAME}
       Topic Name: ${TOPIC_NAME}
       Replication Factor: ${REPLICATION_FACTOR}
       "
+if [ ! -z "${KAFKA_INSTANCE_NAME}" ]; then
+  kafka_uri=$(echo ${KAFKA_SERVICE}:${KAFKA_PORT}/${KAFKA_INSTANCE_NAME})
+else
+  kafka_uri=$(echo ${KAFKA_SERVICE}:${KAFKA_PORT})
+fi
 
-output=$(kafka-topics --bootstrap-server ${KAFKA_SERVICE}:${KAFKA_PORT} --topic ${TOPIC_NAME} --create --partitions 1 --replication-factor ${REPLICATION_FACTOR} --if-not-exists)
+output=$(kafka-topics --bootstrap-server ${kafka_uri} --topic ${TOPIC_NAME} --create --partitions 1 --replication-factor ${REPLICATION_FACTOR} --if-not-exists)
 echo "$now: $output"
